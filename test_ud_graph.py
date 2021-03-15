d9931cbf4f3515148b6d6f30a1966c83b5ddf7f8
richiestuver <richiestuver@protonmail.com>
2021-03-15 14:56:10 -0700

    add unit test file

---
+++ test_ud_graph.py
@@ -0,0 +1,115 @@
+import unittest
+from ud_graph import *
+
+
+class TestAddVertex(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestAddEdge(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestRemoveVertex(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestRemoveEdge(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestGetVertices(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestGetEdges(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestIsValidPath(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestDFS(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestBFS(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestCountConnected(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+
+class TestHasCycle(unittest.TestCase):
+    def test_something(self):
+        expected = []
+        results = []
+
+        for i in range(len(expected)):
+            with self.subTest(expected[i]):
+                self.assertEqual(expected[i], results[i])
+
+if __name__ == '__main__':
+    unittest.main()
